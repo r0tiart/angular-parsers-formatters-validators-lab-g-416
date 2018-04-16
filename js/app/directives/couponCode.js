5476818b090e227ff44d3bd810c4c5cebcbcaad7
r0tiart <antley.chu@gmail.com>
2018-04-16 13:55:11 -0400

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,13 @@
 function couponCode() {
-
+	return{
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, element, attrs, ngModel){
+			ngModel.$validators.coupon = function(value){
+				return /\d{2}[a-z]{4}\d{2}/i.test(value)
+			}
+		}
+	}
 }
 
 angular
--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,5 +1,13 @@
 function makeLower() {
-	
+	return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function (str) {
+                return str.toLowerCase();
+            });
+        }
+    }		
 }
 
 angular
